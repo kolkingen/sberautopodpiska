d3fee0c368097b4d91d4a6a51ba733a974e26f62
Nikolai Borziak <kolkingen@gmail.com>
2022-11-25 13:58:00 +0700

    added statuses for application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
+import logging
+
 from fastapi import FastAPI
 
+from .status import Status
 
+
+# Создание логгера
+log_format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=log_format)
+logger = logging.getLogger(__name__)
+
+# Создание приложения
 app = FastAPI()
+status = Status.NO_MODEL
+logger.debug(f'Приложение создано. Статус: {status.name}')
 
 
 @app.get('/status')
 def get_status() -> str:
+    """Возвращает статус сервиса."""
 
-    return 'Working'
+    logger.debug(f'Запрос статуса сервиса. Статус: {status.name}')
+    return status.value
