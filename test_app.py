3e6725cbda820aa442a582972367932379487f8a
Nikolai Borziak <kolkingen@gmail.com>
2022-12-03 23:16:07 +0700

    added test script and examples

---
+++ test_app.py
@@ -0,0 +1,78 @@
+"""
+Скрипт для быстрой проверки работоспособности приложения.
+Запускать из терминала: 
+```
+    python test_app.py
+```
+"""
+
+
+import time
+import json
+from pathlib import Path
+from pprint import pprint
+from typing import Union
+from urllib.request import urlopen, Request
+
+
+# Адрес сервера и путь к примерам
+app_url = 'http://127.0.0.1:8000'
+examples_file = Path('data', 'examples.json')
+
+
+def send_request(endpoint: str, data: Union[bytes, None] = None) ->None:
+    """Отправляет запрос на сервер и печатает ответ."""
+
+    start_time = time.time()
+    if data is not None:
+        request = Request(app_url + endpoint, data=data, method='POST', 
+                          headers={"Content-Type": "application/json"})
+    else:
+        request = Request(app_url + endpoint)
+    with urlopen(request) as response:
+        result = json.loads(response.read())
+    pprint(result)
+    print(f'Время запроса {time.time() - start_time} секунд')
+    print('-' * 80)
+
+
+def test_app() -> None:
+    """Отправляет все возможные запросы на сервер и печатает ответ."""
+
+    # Загрузка примеров
+    with open(examples_file, 'r') as file:
+        examples = json.load(file)
+
+    print('-' * 80)
+    print('Тесты запросов к приложению.')
+    print('-' * 80)
+
+    print('Запрос статуса.')
+    send_request('/status')
+
+    print('Запрос метаданных.')
+    send_request('/version')
+
+    print('Запрос предсказания класса для одного объекта.')
+    data = json.dumps(examples[0]).encode("utf-8")
+    send_request('/predict', data)
+
+    print('Запрос предсказания вероятности класса для одного объекта.')
+    data = json.dumps(examples[1]).encode("utf-8")
+    send_request('/predict_proba', data)
+
+    print('Запрос предсказания класса для множества объектов.')
+    data = json.dumps(examples).encode("utf-8")
+    send_request('/predict_many', data)
+
+    print('Запрос предсказания вероятности класса для множества объектов.')
+    data = json.dumps(examples).encode("utf-8")
+    send_request('/predict_proba_many', data)
+
+
+if __name__ == '__main__':
+
+    try: 
+        test_app()
+    except:
+        print('Непредвиденная ошибка. Проверьте, включен ли сервер с приложением.')
